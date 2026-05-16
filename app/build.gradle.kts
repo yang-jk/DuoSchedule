76611a0e277fc504fade23359313b6236801d2e7
yang-jk <yang-jk@users.noreply.github.com>
2026-05-17 06:22:16 +0800

    fix: version 1.2.0 with versionCode 12000

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.duoschedule"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10200
+        versionCode = 12000
         versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
